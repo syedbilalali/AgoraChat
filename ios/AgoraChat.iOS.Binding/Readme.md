a5e6ca7a149cf95425ec881a892ff473d1ac3642
syedbilalali <syedbilalali614@gmail.com>
2025-05-01 08:30:15 +0530

    xcframework added into ios native with readme files

---
+++ ios/AgoraChat.iOS.Binding/Readme.md
@@ -0,0 +1,4 @@
+## iOS Sharpie Instructions
+
+### Use Sharpie Commands to create ApiDefinitions/ Struct&Enums files.
+1. sharpie bind -sdk iphoneos -output ./ -namespace HotCoffee.AgoraChat.iOS -scope ./AgoraChat.framework/Headers ./AgoraChat.framework/Headers/AgoraChat-Swift.h
---
+++ ios/native/AgoraChat.xcframework/ios-arm64_x86_64-simulator/AgoraChat.framework/Headers/AgoraChat.h
@@ -0,0 +1,17 @@
+//  AgoraChat.h
+//  AgoraChat
+//
+//  Created by xiaoming.li on 23/02/2021.
+//  Copyright © 2017 easemob.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+// In this header, you should import all the public headers of your framework using statements like #import <AgoraChat/PublicHeader.h>
+
+#import "AgoraChatClient.h"
+#import "AgoraChatClientDelegate.h"
+#import "AgoraChatMultiDevicesDelegate.h"
+#import "AgoraChatMessageReactionChange.h"
+#import "AgoraChatMessageReaction.h"
